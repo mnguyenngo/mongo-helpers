3cef3fa672721d1724c8193b3921f309d7e79aed
Nguyen Ngo <mnguyenngo@gmail.com>
2018-04-05 04:17:40 -1000

    Adjusted indentation in init.py

--- mongo_helpers/__init__.py
+++ mongo_helpers/__init__.py
@@ -1,5 +1,1 @@
-from mongo_helpers.mongo_helpers import (
-                                            convert_db_to_df,
-                                            get_db_names,
-                                            get_collection_names
-                                        )
+from mongo_helpers.mongo_helpers import (convert_db_to_df, get_db_names, get_collection_names)
